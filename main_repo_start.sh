780824b49ae4d7ab43d24c27bcebf1f862351396
Frozemint <nami5504@gmail.com>
2021-04-26 15:57:10 -0700

    initial commit

---
+++ main_repo_start.sh
@@ -0,0 +1,4 @@
+docker run -it --rm -p 3000:3000 -v $PWD:/PrairieLearn -v "$HOME/pl_ag_jobs:/jobs" `# Map jobs directory into /jobs` \
+    -e HOST_JOBS_DIR="$HOME/pl_ag_jobs" \
+    -v /var/run/docker.sock:/var/run/docker.sock `# Mount docker into itself so container can spawn others` \
+    prairielearn/prairielearn
